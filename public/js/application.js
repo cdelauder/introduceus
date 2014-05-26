752c710727427f4944a1c537734e8b38d58445ee
Chris DeLauder <cdelauder@gmail.com>
2014-05-25 22:00:42 -0700

    not sure if I will be able to get the javascript out of my html. Havent found a good reference online yet

--- public/js/application.js
+++ public/js/application.js
@@ -2,3 +2,22 @@ $(document).ready(function() {
   bindEvents();
 });
 
+
+function bindEvents() {
+
+}
+
+function stringifyResponse(response) {
+  var me = response
+  debugger
+}
+
+
+// <div id="fb-root"></div>
+// <script>(function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1451067045133237&version=v2.0";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));</script>
