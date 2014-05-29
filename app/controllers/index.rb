c778cc9d696bde1a341434621f794eafe43000b8
Chris DeLauder <cdelauder@gmail.com>
2014-05-29 16:54:39 -0700

    everything is currently working. still trying to implement getting next set of pics

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,7 +4,7 @@ end
 
 post '/login' do
   session[:token] = params[:token]
-  @photo = set_user(session[:token], session[:user_id])
+  @photo = set_user(session[:token])
   content_type JSON
   {photo: @photo}.to_json
 end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,15 +2,19 @@ require 'koala'
 
 helpers do
 
-  def set_user(token, user_id)
+  def set_user(token)
     @graph = Koala::Facebook::API.new(token)
     get_photos(token)
     select_photo
   end
 
   def get_photos(token)
-    photos = @graph.get_connections('me', 'photos')
-    photos =  photos.map {|json_object| json_object['source']}
+    query_result = @graph.get_connections('me', 'photos', {limit: 100})
+    add_photos_to_db(query_result)
+  end
+
+  def add_photos_to_db(query_result)
+    photos =  query_result.map {|json_object| json_object['source']}
     photos.each {|photo| Photo.create(source: photo)}
   end
 
